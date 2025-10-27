6b8cddfc97f0bf7427095ecbd27ff4a76a314416
dinhkhang1 <148904954+dinhkhang1@users.noreply.github.com>
2025-10-27 10:50:45 +0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int tong(int a, int b);
+int hieu(int a, int b);
+int tich(int a, int b);
+float thuong(int a, int b);
+int main()
+{
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	cout << "tong";
+	cin >> a, b;
+	cout << "KQ" << a + b << endl;
+	system("pause");
+	return 0;
+}
